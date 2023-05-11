d8f40a81225b102e306cdc49c7976be7437ee70f
admin@example.com <120072370@qq.com>
2023-05-11 18:01:30 +0800

    create Area class

---
+++ src/main/java/com/gan/project/entity/Area.java
@@ -0,0 +1,68 @@
+package com.gan.project.entity;
+
+import java.util.Date;
+
+public class Area {
+	
+	//ID
+	private Integer areaid;
+
+	//名称
+	private String areaName;
+
+	//权重
+	private Integer priority;
+
+	//创建时间
+	private Date createTime;
+
+	//更新时间
+	private Date lastEditTime;
+
+	public Integer getAreaid() {
+		return areaid;
+	}
+
+	public void setAreaid(Integer areaid) {
+		this.areaid = areaid;
+	}
+
+	public String getAreaName() {
+		return areaName;
+	}
+
+	public void setAreaName(String areaName) {
+		this.areaName = areaName;
+	}
+
+	public Integer getPriority() {
+		return priority;
+	}
+
+	public void setPriority(Integer priority) {
+		this.priority = priority;
+	}
+
+	public Date getCreateTime() {
+		return createTime;
+	}
+
+	public void setCreateTime(Date createTime) {
+		this.createTime = createTime;
+	}
+
+	public Date getLastEditTime() {
+		return lastEditTime;
+	}
+
+	public void setLastEditTime(Date lastEditTime) {
+		this.lastEditTime = lastEditTime;
+	}
+
+
+	
+	
+	
+	
+	
+}
