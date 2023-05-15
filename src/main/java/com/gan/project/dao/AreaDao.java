55985ec56953b24843a70975cb13a3d30871d732
admin@example.com <120072370@qq.com>
2023-05-15 15:50:37 +0800

    no message

--- src/main/java/com/gan/project/dao/AreaDao.java
+++ src/main/java/com/gan/project/dao/AreaDao.java
@@ -10,7 +10,35 @@ public interface AreaDao {
 	 * 列出区域列表
 	 * @return
 	 */
-	 List<Area> queryArea();
+	List<Area> queryArea();
+
+	/**
+	 * 
+	 * @param area
+	 * @return
+	 */
+	int insertArea(Area area);
+
+	/**
+	 * 
+	 * @param area
+	 * @return
+	 */
+	int updateArea(Area area);
+
+	/**
+	 * 
+	 * @param areaId
+	 * @return
+	 */
+	int deleteArea(long areaId);
+
+	/**
+	 * 
+	 * @param areaIdList
+	 * @return
+	 */
+	int batchDeleteArea(List<Long> areaIdList);
 	
 	
 }
